76f7f49eba31e68832c417800ebf24b1d4ed453d
Robin Lavallee <robin.lavallee@implicitconversions.com>
2023-12-16 19:20:00 -0500

    day 16 part 1

---
+++ day16/day16.cpp
@@ -0,0 +1,202 @@
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+#include <set>
+
+unsigned long long findHash(std::string& token) {
+    unsigned long long hash = 0;
+    for (int i = 0; i < token.size(); i++) {
+        hash += token[i];
+        hash *= 17;
+        hash %= 256;
+    }
+
+    return hash;
+}
+
+struct Space {
+    char type;
+    bool directions[4] = { false, false, false, false };
+};
+
+static const int Up = 0;
+static const int Down = 1;
+static const int Left = 2;
+static const int Right = 3;
+
+struct ActiveLight {
+    int x;
+    int y;
+    int direction;
+
+    bool operator<(const ActiveLight& other) const {
+        if (x < other.x) {
+            return true;
+        } else if (x > other.x) {
+            return false;
+        } else if (y < other.y) {
+            return true;
+        } else if (y > other.y) {
+            return false;
+        } else if (direction < other.direction) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+int first() {
+    std::fstream newfile;
+    newfile.open("input.txt", std::ios::in);
+    if (newfile.is_open()) {
+        std::vector<std::vector<Space>> rows;
+
+        std::string line;
+        while (getline(newfile, line)) {
+            std::vector<Space> row;
+            for (int i = 0; i < line.size(); i++) {
+                Space space;
+                space.type = line[i];
+                row.push_back(space);
+            }
+            rows.push_back(row);
+        }
+
+        std::map<ActiveLight, bool> activeLights;
+        activeLights.insert({ { 0, 0, Right }, true });
+        
+        bool veryFirstLight = true;
+        while (activeLights.size() > 0) {
+            ActiveLight originalLight = activeLights.begin()->first;
+            ActiveLight nextLight = originalLight;
+
+            if (!veryFirstLight) {
+                if (nextLight.direction == Up) {
+                    nextLight.y--;
+                } else if (nextLight.direction == Down) {
+                    nextLight.y++;
+                } else if (nextLight.direction == Left) {
+                    nextLight.x--;
+                } else if (nextLight.direction == Right) {
+                    nextLight.x++;
+                }
+            } else {
+                veryFirstLight = false;
+            }
+
+            if (nextLight.x < 0 || nextLight.x >= rows[0].size() || nextLight.y < 0 || nextLight.y >= rows.size()) {
+                activeLights.erase(activeLights.begin());
+                continue;
+            }
+
+            Space& nextSpace = rows[nextLight.y][nextLight.x];
+
+            // Now check if we have been there before in that direction
+            if (nextSpace.directions[nextLight.direction]) {
+                activeLights.erase(activeLights.begin());
+                continue;
+            }
+
+            // Ok, it's a new space, awesome, note that we have entered from a specific direction
+            nextSpace.directions[nextLight.direction] = true;
+
+            // now check what we do next
+            // NOTE: currentLight gets invalidated here because we are adding to an active iterator
+            if (nextLight.direction == Up) {
+                if (nextSpace.type == '/') {
+                    nextLight.direction = Right;
+                } else if (nextSpace.type == '\\') {
+                    nextLight.direction = Left;
+                } else if (nextSpace.type == '-') {
+                    nextLight.direction = Left;
+                    ActiveLight newLight = nextLight;
+                    newLight.direction = Right;
+                    activeLights[newLight] = true;
+                    
+                }
+            } else if (nextLight.direction == Down) {
+                if (nextSpace.type == '/') {
+                    nextLight.direction = Left;
+                } else if (nextSpace.type == '\\') {
+                    nextLight.direction = Right;
+                } else if (nextSpace.type == '-') {
+                    nextLight.direction = Right;
+                    ActiveLight newLight = nextLight;
+                    newLight.direction = Left;
+                    activeLights[newLight] = true;
+                }
+            } else if (nextLight.direction == Left) {
+                if (nextSpace.type == '/') {
+                    nextLight.direction = Down;
+                } else if (nextSpace.type == '\\') {
+                    nextLight.direction = Up;
+                } else if (nextSpace.type == '|') {
+                    nextLight.direction = Up;
+                    ActiveLight newLight = nextLight;
+                    newLight.direction = Down;
+                    activeLights[newLight] = true;
+                }
+            } else if (nextLight.direction == Right) {
+                if (nextSpace.type == '/') {
+                    nextLight.direction = Up;
+                } else if (nextSpace.type == '\\') {
+                    nextLight.direction = Down;
+                } else if (nextSpace.type == '|') {
+                    nextLight.direction = Down;
+                    ActiveLight newLight = nextLight;
+                    newLight.direction = Up;
+                    activeLights[newLight] = true;
+                }
+            }
+
+            activeLights.erase(originalLight);
+            activeLights[nextLight] = true;
+        }
+        
+        // count the number of energized light, this will be the number of spaces with any direction set
+        int numEnergized = 0;
+        for (int j = 0; j < rows.size(); j++) {
+            for (int i = 0; i < rows[j].size(); i++) {
+                Space& space = rows[j][i];
+                if (space.directions[Up] || space.directions[Down] || space.directions[Left] || space.directions[Right]) {
+                    std::cout << "#";
+                    numEnergized++;
+                } else {
+                    std::cout << ".";
+                }
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << numEnergized << std::endl;
+
+        newfile.close();
+    }
+
+    return 0;
+}
+
+
+int second() {
+    std::fstream newfile;
+    newfile.open("input.txt", std::ios::in);
+    if (newfile.is_open()) {
+        
+        newfile.close();
+    }
+
+    return 0;
+}
+
+int main() {
+    first();
+    second();
+}
+
+
