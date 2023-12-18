6f28d7bf5e9c52746d9579c765e700dcd60fd55a
Robin Lavallee <robin.lavallee@implicitconversions.com>
2023-12-17 20:08:07 -0500

    need to debug algo

--- day17/day17.cpp
+++ day17/day17.cpp
@@ -17,17 +17,81 @@ static const int Right = 3;
 struct Point {
     int x;
     int y;
+
+    bool operator<(const Point& other) const {
+        if (x < other.x) {
+            return true;
+        } else if (x > other.x) {
+            return false;
+        } else if (y < other.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool operator>(const Point& other) const {
+        if (x > other.x) {
+            return true;
+        } else if (x < other.x) {
+            return false;
+        } else if (y > other.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 struct DirectionCount {
     int direction = None;
     int count = 0;
+
+    bool operator<(const DirectionCount& other) const {
+        if (direction < other.direction) {
+            return true;
+        } else if (direction > other.direction) {
+            return false;
+        } else if (count < other.count) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool operator>(const DirectionCount& other) const {
+        if (direction > other.direction) {
+            return true;
+        } else if (direction < other.direction) {
+            return false;
+        } else if (count > other.count) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 struct PointDirection {
-     Point point;
-     DirectionCount dirCount;
-     int cost = 0;
+    Point point;
+    DirectionCount dirCount;
+    int cost = 0;
+
+    bool operator<(const PointDirection& other) const {
+        if (point < other.point) {
+            return true;
+        } else if (point > other.point) {
+            return false;
+        } else if (dirCount < other.dirCount) {
+            return true;
+        } else if (dirCount > other.dirCount) {
+            return false;
+        } else if (cost < other.cost) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 
@@ -83,18 +147,99 @@ int solve(std::vector<std::vector<int>> maze) {
 
                 nextDirectionCount.cost += maze[nextDirectionCount.point.y][nextDirectionCount.point.x];
 
-                auto visitedIt = visited[nextDirectionCount.point.y][nextDirectionCount.point.x].find(nextDirectionCount.dirCount);
-                if (visitedIt == visited[nextDirectionCount.point.y][nextDirectionCount.point.x].end()) {
-                    visited[nextDirectionCount.point.y][nextDirectionCount.point.x][nextDirectionCount.dirCount] = nextDirectionCount.cost;
+                auto& visitedEntry = visited[nextDirectionCount.point.y][nextDirectionCount.point.x];
+
+                auto visitedIt = visitedEntry.find(nextDirectionCount.dirCount);
+                if (visitedIt == visitedEntry.end()) {
+                    visitedEntry[nextDirectionCount.dirCount] = nextDirectionCount.cost;
                     toProcess[nextDirectionCount.point][nextDirectionCount.dirCount] = nextDirectionCount.cost;
                 } else {
-                    // TODO
+                    // There cannot be more than 3 entries in the map
+                    bool ignore=false;
+                    for (auto it = visitedEntry.begin(); it != visitedEntry.end();) {
+                        auto& visitedElem = *it;
+
+                        // Replace worse entries (new entry is better on cost and direction count)
+                        if (visitedElem.first.count > nextDirectionCount.dirCount.count && visitedElem.second > nextDirectionCount.cost) {
+                            // Remove from toProcess as well
+                            auto toProcessIt = toProcess[nextDirectionCount.point].find(visitedElem.first);
+                            if (toProcessIt != toProcess[nextDirectionCount.point].end()) {
+                                toProcess[nextDirectionCount.point].erase(toProcessIt);
+                            }
+                            it = visitedEntry.erase(it);
+                            continue;
+                        }
+
+                        // Replace worse entries (new entry is better on cost, and same on direction count)
+                        if (visitedElem.first.count == nextDirectionCount.dirCount.count && visitedElem.second > nextDirectionCount.cost) {
+                            auto toProcessIt = toProcess[nextDirectionCount.point].find(visitedElem.first);
+                            if (toProcessIt != toProcess[nextDirectionCount.point].end()) {
+                                toProcess[nextDirectionCount.point].erase(toProcessIt);
+                            }
+                            it = visitedEntry.erase(it);
+                            continue;
+                        }
+
+                        // Replace worse entries (new entry is same on cost, and better on direction count)
+                        if (visitedElem.first.count < nextDirectionCount.dirCount.count && visitedElem.second == nextDirectionCount.cost) {
+                            auto toProcessIt = toProcess[nextDirectionCount.point].find(visitedElem.first);
+                            if (toProcessIt != toProcess[nextDirectionCount.point].end()) {
+                                toProcess[nextDirectionCount.point].erase(toProcessIt);
+                            }
+                            it = visitedEntry.erase(it);
+                            continue;
+                        }
+
+                        // do nothing if the entry is the same (ignore it)
+                        if (visitedElem.first.count == nextDirectionCount.dirCount.count && visitedElem.second == nextDirectionCount.cost) {
+                            ignore = true;
+                            ++it;
+                            continue;
+                        }
+
+                        // do nothing if the entry has same cost but higher count
+                        if (visitedElem.second == nextDirectionCount.cost && visitedElem.first.count > nextDirectionCount.dirCount.count) {
+                            ignore = true;
+                            ++it;
+                            continue;
+                        }
+
+                        // do nothing if the entry has higher cost but same count
+                        if (visitedElem.second > nextDirectionCount.cost && visitedElem.first.count == nextDirectionCount.dirCount.count) {
+                            ignore = true;
+                            ++it;
+                            continue;
+                        }
+
+                        // do nothing if the entry has higher cost and higher count
+                        if (visitedElem.second > nextDirectionCount.cost && visitedElem.first.count > nextDirectionCount.dirCount.count) {
+                            ignore = true;
+                            ++it;
+                            continue;
+                        }
+                        ++it;
+                    }
+
+                    if (!ignore) {
+                        visitedEntry[nextDirectionCount.dirCount] = nextDirectionCount.cost;
+                        toProcess[nextDirectionCount.point][nextDirectionCount.dirCount] = nextDirectionCount.cost;
+                    }
                 }
             }
         }
+
+        toProcess.erase(pointCandidate);
     }
 
-    return 0;
+    // get the best cost from bottom right position
+    int bestCost = INT_MAX;
+    for (auto& entry : visited[maze.size() - 1][maze[0].size() - 1]) {
+        if (entry.second < bestCost) {
+            bestCost = entry.second;
+        }
+    }
+
+    return bestCost;
 }
 
 int first() {
