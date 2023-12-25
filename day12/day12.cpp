fdaa2d8b47d8d51a7c9b720dddc3b2e92d75eba2
Robin Lavallee <robin.lavallee@implicitconversions.com>
2023-12-25 13:42:24 -0500

    Day 12 done

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -172,8 +172,52 @@ int first() {
     return 0;
 }
 
-int computeArrangements(std::string record, std::vector<int> numbers) {
+struct CacheEntry {
+    std::string record;
+    std::vector<int> numbers;
+
+    bool operator<(const CacheEntry& other) const
+    {
+        if (record < other.record) {
+            return true;
+        }
+
+        if (record > other.record) {
+            return false;
+        }
+
+        if (numbers.size() < other.numbers.size()) {
+            return true;
+        }
+
+        if (numbers.size() > other.numbers.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < numbers.size(); ++i) {
+            if (numbers[i] < other.numbers[i]) {
+                return true;
+            }
+
+            if (numbers[i] > other.numbers[i]) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+};
+
+std::map<CacheEntry, unsigned long long> cache;
+
+unsigned long long computeArrangements(std::string record, std::vector<int> numbers) {
+    CacheEntry entry = { record, numbers };
+    if (cache.find(entry) != cache.end()) {
+        return cache[entry];
+    }
+
     if (numbers.size() == 0) {
+        cache[entry] = 0;
         return 0;
     }
     
@@ -184,6 +228,7 @@ int computeArrangements(std::string record, std::vector<int> numbers) {
 
     numRequired += numbers.size() - 1;
     if (numRequired > record.size()) {
+        cache[entry] = 0;
         return 0;
     }
 
@@ -191,7 +236,7 @@ int computeArrangements(std::string record, std::vector<int> numbers) {
 
     // If we want to do 1, then we need to do #. so we can look everywhere and place it, if we see anything else like ##., then it doesn't work, then recurse
 
-    int totalArrangements = 0;
+    unsigned long long totalArrangements = 0;
     int number = numbers[0];
     for (int i = 0; i < record.size()-number+1; ++i) {
 
@@ -251,11 +296,12 @@ int computeArrangements(std::string record, std::vector<int> numbers) {
                 continue;
             }
 
-            int numArrangement = computeArrangements(record.substr(i + number + 1), std::vector<int>(numbers.begin() + 1, numbers.end()));
+            unsigned long long numArrangement = computeArrangements(record.substr(i + number + 1), std::vector<int>(numbers.begin() + 1, numbers.end()));
             totalArrangements += numArrangement;
         }
     }
 
+    cache[entry] = totalArrangements;
     return totalArrangements;
 }
 
@@ -291,7 +337,7 @@ int firstalternative() {
                 space = rest.find(",");
             }
 
-            int numArrangement = computeArrangements(record, numbers);
+            unsigned long long numArrangement = computeArrangements(record, numbers);
             std::cout << numArrangement << std::endl;
             totalValidArrangements += numArrangement;
         }
@@ -300,7 +346,7 @@ int firstalternative() {
             
         newfile.close();
     }
-       
+      
 
     return 0;
 }
@@ -348,130 +394,9 @@ int second() {
 
             std::swap(newNumbers, numbers);
 
-            // basically the numbers are telling us how many '1's bits we have
-            // the # are already 1s
-            // the . are already 0s
-            // the number of commas tell us how many regions with 0s we have
-
-            // ???.###    and 1,1,3
-            // we know we need 5 bits and we already have 3
-            // there are 2 bits left out of 3 arrangement
-            // so we can try all combinations of 2 bits out of 3, basically looping over 0 to 2^3-1 and then applying the bits from left to right
-            // then we can count the sequence of bits and see if matches the expectation
-            // so it would go like:
-            // 000 (wrong)
-            // 001 (wrong)
-            // 010 (wrong)
-            // 011 (possible)   --> 011.111  --> 2, 3 -> wrong
-            // 100 (wrong)
-            // 101 (possible)   --> 101.111  --> 1, 1, 3 -> right (SUCCESS )
-            // 110 (possible)   --> 110.111  --> 2, 3 -> wrong
-            // 111 (wrong)      
-
-            int howManyBits = record.size();
-            int howManyOnes = 0;
-            for (int i = 0; i < record.size(); ++i) {
-                if (record[i] == '#') {
-                    howManyOnes++;
-                }
-            }
-
-            int howManyOnesNeeded = 0;
-            for (int i = 0; i < numbers.size(); ++i) {
-                howManyOnesNeeded += numbers[i];
-            }
-
-            int howManyQuestions = 0;
-            for (int i = 0; i < record.size(); ++i) {
-                if (record[i] == '?') {
-                    howManyQuestions++;
-                }
-            }
-
-            unsigned long long max = 1ULL << howManyQuestions;
-            int onesRemaining = howManyOnesNeeded - howManyOnes;
-            unsigned long long numValidArrangements = 0;
-            for (unsigned long long i = 0; i < max; ++i) {
-                // count how many bits are set in i
-                int bitsSet = 0;
-                unsigned long long temp = i;
-                while (temp > 0) {
-                    if (temp & 1) {
-                        bitsSet++;
-                        if (bitsSet > onesRemaining)
-                            break;
-                    }
-                    temp >>= 1;
-                }
-
-                if (bitsSet != onesRemaining) {
-                    continue;
-                }
-
-                // ???.###
-                // go from left to right, and for each ? replace with the next bit in i
-                int bitIndex = 0;
-                std::string newRecord = "";
-                for (int j = 0; j < record.size(); ++j) {
-                    if (record[j] == '?') {
-                        if (i & (1ULL << bitIndex)) {
-                            newRecord += '#';
-                        } else {
-                            newRecord += '.';
-                        }
-                        bitIndex++;
-                    } else {
-                        newRecord += record[j];
-                    }
-                }
-
-                // now check if newRecord bits sequence matches the expectation in numbers
-                int currentSequence = 0;
-                int expectedSequence = 0;
-                bool valid = true;
-                for (int j = 0; j < newRecord.size(); ++j) {
-                    if (newRecord[j] == '#') {
-                        currentSequence++;
-                    } else {
-                        if (currentSequence > 0) {
-                            expectedSequence++;
-                            if (expectedSequence > numbers.size()) {
-                                valid = false;
-                                break;
-                            }
-                            if (currentSequence != numbers[expectedSequence - 1]) {
-                                valid = false;
-                                break;
-                            }
-                            currentSequence = 0;
-                        }
-                    }
-                }
-
-                if (!valid) {
-                    continue;
-                }
-
-                if (currentSequence > 0) {
-                    expectedSequence++;
-                    if (expectedSequence > numbers.size()) {
-                        continue;
-                    }
-                    if (currentSequence != numbers[expectedSequence - 1]) {
-                        continue;
-                    }
-                }
-
-                // I think we need to fix the last number
-
-                if (expectedSequence == numbers.size()) {
-                    //std::cout << newRecord << std::endl;
-                    numValidArrangements++;
-                }
-            }
-
-            std::cout << numValidArrangements << std::endl;
-            totalValidArrangements += numValidArrangements;
+            unsigned long long numArrangement = computeArrangements(record, numbers);
+            //std::cout << numArrangement << std::endl;
+            totalValidArrangements += numArrangement;
         }
 
         std::cout << totalValidArrangements << std::endl;
@@ -484,10 +409,8 @@ int second() {
 
 int main() {
     //first();
-    firstalternative();
-    //second();
-
-    
+    //firstalternative();
+    second();
 }
 
 
